bac195b11d70781a33f9663bf792d558e16044a0
Aaron Smith <smitham50@gmail.com>
2019-07-07 13:12:16 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^0-9a-zA-Z '-]/g, '')
+  }
+
+  static titleize(string) {
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let splitString = string.split(" ")
+    let stringMap = splitString.map(function(word) {
+      if (exceptions.includes(word) && word !== splitString[0]) {
+        return word
+      }
+      else {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+      }
+    })
+    return stringMap.join(" ")
+  }
 }
+
